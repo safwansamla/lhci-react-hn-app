d074b779c97942f272336811c335d796abef6c87
safwansamla <safwans@google.com>
2020-04-24 13:31:27 -0400

    Initial commit

---
+++ src/mixins/ItemMixin.js
@@ -0,0 +1,85 @@
+var React = require('react')
+var Link = require('react-router/lib/Link')
+var TimeAgo = require('react-timeago').default
+
+var SettingsStore = require('../stores/SettingsStore').default
+var pluralise = require('../utils/pluralise').default
+var urlParse = require('url-parse')
+
+var parseHost = function(url) {
+  var hostname = (urlParse(url, true)).hostname
+  var parts = hostname.split('.').slice(-3)
+  if (parts[0] === 'www') {
+    parts.shift()
+  }
+  return parts.join('.')
+}
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+/**
+ * Reusable logic for displaying an item.
+ */
+var ItemMixin = {
+  /**
+   * Render an item's metadata bar.
+   */
+  renderItemMeta(item, extraContent) {
+    var itemDate = new Date(item.time * 1000)
+
+    if (item.type === 'job') {
+      return <div className="Item__meta">
+        <TimeAgo date={itemDate} className="Item__time"/>
+      </div>
+    }
+
+    // Add these lines before extraContent to introduce delay
+    //<img src='http://getwallpapers.com/wallpaper/full/5/7/b/512574.jpg' width="240"/>
+    //{wait(400)}
+
+    return <div className="Item__meta">
+      <span className="Item__score">
+        {item.score} point{pluralise(item.score)}
+      </span>{' '}
+      <span className="Item__by">
+        by <Link to={`/user/${item.by}`}>{item.by}</Link>
+      </span>{' '}
+      <TimeAgo date={itemDate} className="Item__time"/>
+      {' | '}
+      <Link to={`/${item.type}/${item.id}`}>
+        {item.descendants > 0 ? item.descendants + ' comment' + pluralise(item.descendants) : 'discuss'}
+      </Link>
+      <br />
+
+
+      {extraContent}
+    </div>
+  },
+
+  /**
+   * Render an item's title bar.
+   */
+  renderItemTitle(item) {
+    var hasURL = !!item.url
+    var title
+    if (item.dead) {
+      title = '[dead] ' + item.title
+    }
+    else {
+      title = (hasURL ? <a href={item.url}>{item.title}</a>
+        : <Link to={`/${item.type}/${item.id}`}>{item.title}</Link>)
+    }
+    return <div className="Item__title" style={{fontSize: `${SettingsStore.titleFontSize}px`}}>
+      {title}
+      {hasURL && ' '}
+      {hasURL && <span className="Item__host">({parseHost(item.url)})</span>}
+    </div>
+  }
+}
+
+export default ItemMixin
